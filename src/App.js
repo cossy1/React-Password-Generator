f74e02bed2c2c4b667810b92601bf565dd30cfa6
cossy1 <onyekwuocosmas@gmail.com>
2020-12-25 09:44:16 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import PasswordGenerator from "./components/password";
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <PasswordGenerator />
+      
+    </div>
+  );
+}
+
+export default App;
